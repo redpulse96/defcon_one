64cc58b7a6dcf20777b9eb3a069fcd368a5d4429
Syed Mohammed Redhan Kamal <syed.k@servify.in>
2019-11-30 01:11:22 +0530

    patient based fulfilment

--- controllers/appointments/appointments.js
+++ controllers/appointments/appointments.js
@@ -1,7 +1,6 @@
 const Appointments = require(packageHelper.MODEL_CONFIG_DIR)['Appointments'];
 const AppointmentLogs = require(packageHelper.MODEL_CONFIG_DIR)['AppointmentLogs'];
 
-require('./fetch_appointment')(Appointments);
 require('./create_appointment')(Appointments);
 require('./status_based_appointments')(Appointments);
 require('./appointment_fulfilment')(Appointments, AppointmentLogs);
