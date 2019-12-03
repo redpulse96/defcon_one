86386b39a5d482f887b9bbefa198b0446e846082
Syed Mohammed Redhan Kamal <syed.k@servify.in>
2019-12-03 22:14:44 +0530

    fixed linter errors

--- controllers/patients/create_patient.js
+++ controllers/patients/create_patient.js
@@ -58,42 +58,44 @@ module.exports = Patients => {
       let createObj = Object.assign({}, req.body, {
         created_by: utils.validateKeys(() => req.user.username, null, null)
       });
-      models['Patients'].scope('activeScope').findOrCreate({
-          where: {
-            mobile_no: createObj.mobile_no
-          },
-          defaults: createObj
-        })
-        .then(([create_res, is_new]) => {
-          log.info('---PATIENTS_CREATION_SUCCESS---');
-          log.info(create_res, is_new);
-          if (is_new) {
-            return callback(null, {
-              success: true,
-              message: 'Patients creation success',
-              data: {
-                patient: create_res.toJSON()
-              }
-            });
-          } else {
-            return callback({
-              success: false,
-              error_code: 400,
-              message: 'Mobile number already exists,\nkindly priovide a different mobile number',
-              data: {}
-            });
-          }
-        })
-        .catch(create_err => {
-          log.error('---PATIENTS_CREATION_FAILURE---');
-          log.error(create_err);
+      models['Patients']
+      .scope('activeScope')
+      .findOrCreate({
+        where: {
+          mobile_no: createObj.mobile_no
+        },
+        defaults: createObj
+      })
+      .then(([create_res, is_new]) => {
+        log.info('---PATIENTS_CREATION_SUCCESS---');
+        log.info(create_res, is_new);
+        if (is_new) {
+          return callback(null, {
+            success: true,
+            message: 'Patients creation success',
+            data: {
+              patient: create_res.toJSON()
+            }
+          });
+        } else {
           return callback({
             success: false,
-            error_code: 500,
-            message: 'Patients creation failure',
+            error_code: 400,
+            message: 'Mobile number already exists,\nkindly priovide a different mobile number',
             data: {}
           });
+        }
+      })
+      .catch(create_err => {
+        log.error('---PATIENTS_CREATION_FAILURE---');
+        log.error(create_err);
+        return callback({
+          success: false,
+          error_code: 500,
+          message: 'Patients creation failure',
+          data: {}
         });
+      });
     }
   }
 }
