33689173ec62535f60f966428e041903087b57d3
Syed Mohammed Redhan Kamal <syed.k@servify.in>
2019-12-09 22:47:51 +0530

    logger middleware changed

--- config/middleware/log_middleware.js
+++ config/middleware/log_middleware.js
@@ -1,12 +1,12 @@
 const log = require('../log_config').logger('log_middleware');
 
 const apiLogger = (req, res, next) => {
-  log.info('<<<<===============API_HEADERS===============>>>>');
-  log.info(req.headers);
-  log.info('<<<<===============API_METHOD===============>>>>');
-  log.info(req.method);
   log.info('<<<<===============API_PATH===============>>>>');
   log.info(req.path);
+  log.info('<<<<===============API_METHOD===============>>>>');
+  log.info(req.method);
+  log.info('<<<<===============API_HEADERS===============>>>>');
+  log.info(req.headers);
   log.info('<<<<===============API_PAYLOAD_START===============>>>>');
   log.info(req.body || req.params);
   log.info('<<<<===============API_PAYLOAD_END===============>>>>');
