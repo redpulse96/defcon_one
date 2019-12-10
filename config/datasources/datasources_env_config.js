acc3b30b722465dcab241a52c8c3c38baff8eb12
Syed Mohammed Redhan Kamal <syed.k@servify.in>
2019-12-11 00:24:38 +0530

    db logging disabled

--- config/datasources/datasources_env_config.js
+++ config/datasources/datasources_env_config.js
@@ -34,7 +34,7 @@ mysql_config = {
     host: datasources[env].mysql.host,
     port: datasources[env].mysql.port,
     operatorsAliases: operatorsAliases,
-    logging: true,
+    logging: false,
     pool: {
       max: 30,
       min: 0,
