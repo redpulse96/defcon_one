40f8639a01ebc6204676da4d035837232866b90a
Syed Mohammed Redhan Kamal <impulse96@icloud.com>
2019-12-29 20:25:16 +0530

    minor fixes (#21)

--- controllers/utility/helper_function.js
+++ controllers/utility/helper_function.js
@@ -1,16 +1,26 @@
 const log = require('../../config/log_config').logger('helper_function');
 
 module.exports = {
+  /**
+   * @param {Promise} promise - Takes a promise to be fulfilled
+   */
   to: promise => promise.then(data => ([null, data])).catch(err => ([err, null])),
+  /**
+   * @param {Object} objectFn - List of all the helper functions related to an object
+   */
   objectFn: {
+    /**
+     * @param {Object} obj - Object to check the presence of the key
+     * @param {String} key - Key to be checked if present in the object
+     */
     hasFn: (obj, key) => {
       let is_exists;
       obj[key] ? is_exists = true : is_exists = false;
       return is_exists;
     },
     /**
-     * @param {Object} parent_obj - Array of objects to group
-     * @param {Object} child_obj - Array of objects to group
+     * @param {Object} parent_obj - Parent object to be merged
+     * @param {Object} child_obj - Child object to be merged
      */
     mergeFn: (parent_obj, child_obj) => {
       let res_obj = {
@@ -20,6 +30,9 @@ module.exports = {
       return res_obj;
     }
   },
+  /**
+   * @param {Object} arrayFn - List of all the helper functions related to an array
+   */
   arrayFn: {
     /**
      * @param {Array} arr - Array of objects to map a key from
