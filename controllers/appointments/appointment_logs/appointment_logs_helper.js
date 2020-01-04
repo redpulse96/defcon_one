52acef9cab5db1b9f4a70c6937a8cf1cc44ad746
syed <syed.k@servify.in>
2020-01-04 11:42:44 +0530

    log config require fix

--- controllers/appointments/appointment_logs/appointment_logs_helper.js
+++ controllers/appointments/appointment_logs/appointment_logs_helper.js
@@ -1,4 +1,4 @@
-const log = require('../../config/log_config').logger('appointment_logs_helper');
+const log = require('../../../config/log_config').logger('appointment_logs_helper');
 const {
   objectFn
 } = require('../../utility/helper_function');
